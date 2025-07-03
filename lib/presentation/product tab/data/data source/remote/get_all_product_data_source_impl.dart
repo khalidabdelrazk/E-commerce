1abc6b1b648f14bb18bae0d0dfaf42d950d19cec
khalidabdelrazk <khalidabdelrazk4@gmail.com>
2025-07-03 16:55:34 +0300

    product implementation

---
+++ lib/presentation/product tab/data/data source/remote/get_all_product_data_source_impl.dart
@@ -0,0 +1,5 @@
+import '../get_all_product_data_source.dart';
+
+class GetAllProductDataSourceImpl extends GetALlProductDataSource{
+
+}
---
+++ lib/presentation/product tab/data/repository/get_all_product_repository_impl.dart
@@ -0,0 +1,5 @@
+import 'package:ecommerce/presentation/product%20tab/domain/repository/get_all_product_repository.dart';
+
+class GetALlProductRepositoryImpl extends GetAllProductRepository{
+
+}
